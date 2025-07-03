2302024997a991b4c124cf0f71495649acc666ac
kakamigo <gomarketsaja@gmail.com>
2025-07-03 15:55:05 +0700

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,2 +1,25 @@
-if True:
-    sendTelegramMessage("🔥 Uji coba kirim pesan Telegram berhasil!")
+import requests
+from datetime import datetime
+
+TOKEN = "7843209309:AAHT95IIJ0hQ6kHOC8crQtMYbOldb-BQH9w"
+CHAT_ID = "6152549114"
+
+def sendTelegramMessage(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": text
+    }
+    requests.post(url, json=payload)
+
+# Cek waktu lokal Indonesia (UTC+7)
+now = datetime.utcnow().hour + 7
+
+if now == 7:
+    sendTelegramMessage("📊 *Laporan Pagi Otomatis*\n\n(scan pagi otomatis dikirim)")
+elif now == 18:
+    sendTelegramMessage("📊 *Laporan Sore Otomatis*\n\n(scan sore otomatis dikirim)")
+
+
+
+sendTelegramMessage("🔥 Uji coba kirim pesan Telegram berhasil!")
